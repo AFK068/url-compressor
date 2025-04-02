667f015d9d32269560521d65836d9fc741d1fc16
Ivan Gobets <6_6_6_66@mail.ru>
2025-04-02 23:19:04 +0300

    feat: :sparkles: add repository type

--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -2,6 +2,13 @@ package domain
 
 import "context"
 
+type RepositoryType string
+
+const (
+	PostgresRepository RepositoryType = "postgres"
+	InMemoryRepository RepositoryType = "inmemory"
+)
+
 type Repository interface {
 	SaveURL(ctx context.Context, originalURL string) (string, error)
 	GetURL(ctx context.Context, shortenedURL string) (string, error)
