9df5ad8e78616f84559a0679dac485acf0f705de
Ivan Gobets <6_6_6_66@mail.ru>
2025-04-03 00:28:30 +0300

    perf: :zap: shortener error handling

--- pkg/shortener/errors.go
+++ pkg/shortener/errors.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrInvalidLength         = errors.New("length must be greater than 0")
+	ErrInvalidDecoderLength  = errors.New("length must be equal to the specified value")
 	ErrInvalidLengthAlphabet = errors.New("alphabet must not be empty")
 	ErrInvalidStringLength   = errors.New("invalid string length")
 	ErrInvalidCharacter      = errors.New("invalid character in string")
